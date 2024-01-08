c8d0d60b1fac0f988a85ef42b972623849debb6a
윤태현 <yoonth0919@gmail.com>
2024-01-08 22:17:51 +0900

    Chore: cors 로컬, 실도메인 연결

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://woongjin-web-builder.netlify.app"],
     credentials: true,
   })
 );
